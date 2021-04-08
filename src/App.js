1765eccf1f6109d81e47d7c7abf3487802d4e183
Aleksander Barsukov <ajbarsukov@gmail.com>
2021-04-08 11:19:01 +0200

    predeploy

--- src/App.js
+++ src/App.js
@@ -22,6 +22,8 @@ componentDidMount(){
         const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
         return ( 
         <div className = "App"> 
+                <h1> Monsters Rolodex</h1>
+
         <SearchBox
         placeholder="Search Monsters"
         handleChange={
